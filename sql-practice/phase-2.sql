e6daf8f17fa9df0e4226447992520420d3a74654
William <byweimar@gmail.com>
2024-01-16 21:13:45 -0500

    phase 2

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,22 @@
--- Your code here 
+.mode box
+
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40) NOT NULL,
+    phone INTEGER UNIQUE CHECK (length(phone)=10),
+    email VARCHAR(255) UNIQUE,
+    points INTEGER NOT NULL DEFAULT 5,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_orders (
+    id INTEGER PRIMARY KEY,
+    is_redeemed BOOLEAN DEFAULT 0,
+    ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO coffee_orders(id)
+VALUES (1);
